48d92f9c314f838652dbbe90c8b3e509df30758c
YZK0518 <1479236364@qq.com>
2019-07-05 14:25:36 +0800

    add SQL_Server_DataBase_Code to perfect code

---
+++ SQL_Server_DataBase_Code/SQL Server数据库代码/MusicPlayerMusicSql.sql
@@ -0,0 +1,17 @@
+CREATE TABLE MUSICPLAYERMUSIC(
+MUSICNO INT,
+MUSICNAME CHAR(1024),
+MUSICPATH CHAR(4096)
+);
+
+INSERT 
+INTO MUSICPLAYERMUSIC VALUES(1,'Take%%me%%hand.mp3','MusicFile/Take%%me%%hand.mp3');
+
+INSERT 
+INTO MUSICPLAYERMUSIC VALUES(2,'Epic%%Score-Freedom%%Is%%Ours.mp3','MusicFile/Epic%%Score-Freedom%%Is%%Ours.mp3');
+
+INSERT 
+INTO MUSICPLAYERMUSIC VALUES(3,'Sara%%Farell%%-%%Faded.mp3','MusicFile/Sara%%Farell%%-%%Faded.mp3');
+
+INSERT 
+INTO MUSICPLAYERMUSIC VALUES(4,'Lisa%%Miskovsky-Still%%Alive%%(The%%Theme%%From%%Mirrors%%Edge).mp3','MusicFile/Lisa%%Miskovsky-Still%%Alive%%(The%%Theme%%From%%Mirrors%%Edge).mp3');
